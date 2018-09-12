5f0b489bbaf57c17445407b890c7b824ccdeedf7
Snapdragon <34214093+eryvek@users.noreply.github.com>
2018-09-12 23:22:33 +0530

    Update urls.py

--- urls.py
+++ urls.py
@@ -6,6 +6,7 @@ from main import views
 urlpatterns = [
     url(r'^$', TemplateView.as_view(template_name ='home.html'), name="home"),
     url(r'^tweet_by_word',TemplateView.as_view(template_name = 'tweet_by_word.html'),name="tweet_by_word_input"),
+    url(r'^tweet_by_user',TemplateView.as_view(template_name = 'tweet_by_user.html'),name="tweet_by_user_input"),
     url(r'^text_analysis',TemplateView.as_view(template_name = 'text_analysis.html'),name="text_analysis"),
     url(r'^tweets/', views.tweet_by_word, name='tweet_by_word_print'),
     url(r'^admin/', admin.site.urls),
